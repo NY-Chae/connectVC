4de376f7ecd15b3da536196726e526be77ca5db0
Nayeon Chae <cny1230@naver.com>
2024-04-19 16:34:26 +0900

    VCs connected

--- connectVC/connectVC/FirstViewController.swift
+++ connectVC/connectVC/FirstViewController.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- connectVC/connectVC/SecondViewController.swift
+++ connectVC/connectVC/SecondViewController.swift
@@ -1,4 +1,29 @@
+//
+//  SecondViewController.swift
+//  connectVC
+//
+//  Created by 채나연 on 4/19/24.
+//
 
-
-import Foundation
 import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
